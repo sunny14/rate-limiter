3a1491febee08ec161bf1c0a4fa144cdf4cf4408
sunny14 <sunnybunny14@gmail.com>
2020-02-28 09:39:48 +0200

    simple springboot project added

---
+++ src/test/java/sunny14/ratelimiter/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sunny14.ratelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
